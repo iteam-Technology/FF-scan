da5692edf4e2f20f2fb9776f2631003abdd6bad6
iteam-Technology <iteam@iteamservices.com>
2015-02-08 20:15:42 -0500

    Create ffscan.py

---
+++ ffscan.py
@@ -0,0 +1,50 @@
+# Script to find access time and modification of files and folders
+# Author:Vlad
+# Date: February 2015
+
+import os, sys, time
+
+try:
+	from scandir import walk
+except:
+	print('importing os.walk instead of scandir.walk) #scandir.walk is much faster than os.walk
+	from os import walk
+	
+class KeyboardinterruptError(Exception): pass
+
+def main
+	
+	currdir = os.getcwd()
+	
+	print('\n Scanning folders %s for files and %s folders' % (args.files, args.folder))
+	for root, folders, files in dirwalk(args.files, args.folder)
+		try:
+			if os.path.exists()
+			print "The directory is found "
+		except:
+			print "The directory was not found. "
+			
+	
+def dirwalk(top):
+	for root, folders, files in walk(top, topdown = true, oneerror=walkerror):
+		yield root, folders, files
+
+def walkerror(oserror):		
+	sys.stderr.write(str(oserror))
+	sys.stderr.write('\n')
+	return 0
+	
+def accessNMod(st):
+ctime,atime,mtime = st 
+
+	print "Created: ", time.ctime(ctime)
+	print "Last accessed: ", time.ctime(atime)
+	print "Last modified: ", time.ctime(mtime)
+
+st = os.stat(dirwalk())
+
+print "%s Current directory for file access and modification is " % (dirwalk())
+accessNMod(dirwalk())	
+
+sys.exit(main())
+	
