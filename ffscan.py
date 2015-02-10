e25a9125e12a6e0c3d56e24891229246483824d5
iteam-Technology <iteam@iteamservices.com>
2015-02-10 16:16:16 -0500

    Update ffscan.py

--- ffscan.py
+++ ffscan.py
@@ -7,62 +7,55 @@ import os, sys, time
 try:
 	from scandir import walk
 except:
-	print('importing os.walk instead of scandir.walk) #scandir.walk is much faster than os.walk
+	print('importing os.walk instead of scandir.walk') ##scandir.walk is much faster than os.walk
 	from os import walk
 	
 class KeyboardinterruptError(Exception): pass
 
 def main
-	
 	currdir = os.getcwd()
-	
 	print('\n Scanning folders %s for files and %s folders' % (args.files, args.folder))
 	for root, folders, files in dirwalk(args.files, args.folder)
 		try:
 			if os.path.exists()
-			print "The directory is found "
-		except:
-			print "The directory was not found. "
-			
+				print "The directory is found "
+			except:
+				print "The directory was not found. "
+				
 FullFilePaths = get_fullpaths(directory)
 
-print "%s Current directory for file access and modification is " % (FullFilePaths)
-accessNMod(FullFilePaths):				
-	
+	print "%s Current directory for file access and modification is " % (FullFilePaths)
+	accessNMod(FullFilePaths):
+		
 def dirwalk(top):
 	for root, folders, files in walk(top, topdown = true, oneerror=walkerror):
-		yield root, folders, files
-
-def walkerror(oserror):		
+	yield root, folders, files
+	
+def walkerror(oserror):
 	sys.stderr.write(str(oserror))
 	sys.stderr.write('\n')
 	return 0
-	
-def accessNMod(st, directory):
-ctime,atime,mtime = st
-	
-	def get_fullpath(directory):
-		filePaths = [] 
-		folderPaths = []
 
+def accessNMod(st, directory):
+	ctime,atime,mtime = st
+
+	def get_fullpath(directory):
+		filePaths = []
+		folderPaths = []
 		for root, dir, files in dirwalk(directory):
 			for filename in files:
 				filepath = os.path.join(dirpath, filename)
 				filePaths.append(filepath)
-			
 			for folders in dir:
 				folderpath = os.path.join(dirpath, folders)
 				folderPaths.append(folderpaths)
-
-FullFilePaths = get_fullpaths(directory)
-
-
-while (FullFilePaths = true)
-st = os.stat(FullFilePaths)
-
-	print "Created: ", time.ctime(ctime)
-	print "Last accessed: ", time.ctime(atime)
-	print "Last modified: ", time.ctime(mtime)
-
+	FullFilePaths = get_fullpaths(directory)
+	
+	while (FullFilePaths = true)
+		st = os.stat(FullFilePaths)
+		print "Created: ", time.ctime(ctime)
+		print "Last accessed: ", time.ctime(atime)
+		print "Last modified: ", time.ctime(mtime)
 sys.exit(main())
+
 	
